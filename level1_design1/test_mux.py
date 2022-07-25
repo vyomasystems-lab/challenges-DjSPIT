0741a885aae75a5f489cab268f1a37c00998b6a8
Dhananjay Joshi <adidhanu23@gmail.com>
2022-07-25 14:13:35 +0000

    modified:   level1_design1/test_mux.py 	modified:   vyoma.sh

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,9 +2,26 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
 
 @cocotb.test()
-async def test_mux(dut):
-    """Test for mux2"""
-
+async def basic_test_mux(dut):
+    """Basic Test for mux2 - Select Line == 5"""
     cocotb.log.info('##### CTB: Develop your test here ########')
+
+    select_line = 5 # 00101
+    input_val = 10
+
+    dut.inp5.value = input_val
+
+    dut.sel.value = 0
+    yield Timer(10, 'ns')
+    expected = 0
+    if dut.out.value != expected:
+        raise TestError("inp_data={:02}, select_line={:01}, expected value {}, found {}".format(dut.inp5.value, dut.sel.value, expected, dut.out.value))
+
+    dut.sel.value = select_line
+    yield Timer(10, 'ns')
+    expected = input_val
+    if dut.out.value != expected:
+        raise TestError("inp_data={:02}, select_line={:01}, expected value {}, found {}".format(dut.inp5.value, dut.sel.value, expected, dut.out.value))
